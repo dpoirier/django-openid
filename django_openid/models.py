68a0f709d7f3f7e4c4f88f40753dcc64ee3e0d17
Dan Poirier <dpoirier@caktusgroup.com>
2015-07-03 12:25:29 -0400

    Minimal changes to not blow up with Django 1.7
    The code at the end of models.py that looked to see if
    the 'auth.User' model had been loaded before declaring
    another model ran into Django 1.7's better checking that
    you can't look at models until models have been
    initialized.
    
    Modified to just check if 'django.contrib.auth' is in
    INSTALLED_APPS, which is information that's available
    during Django initialization.

--- django_openid/models.py
+++ django_openid/models.py
@@ -118,14 +118,14 @@ class DjangoOpenIDStore(OpenIDStore):
     def isDumb(self):
         return False
 
+
 # Only include table for User->OpenID associations if User model is installed
-user_model = models.get_model('auth', 'User')
-if user_model and user_model._meta.installed:
+if 'django.contrib.auth' in settings.INSTALLED_APPS:
     class UserOpenidAssociation(models.Model):
         "Auth integration - lets you associate 1+ OpenIDs with a User"
-        user = models.ForeignKey('auth.User', related_name = 'openids')
+        user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name = 'openids')
         openid = models.CharField(max_length = 255)
         created = models.DateTimeField(auto_now_add = True)
-        
+
         def __unicode__(self):
             return u'%s can log in with %s' % (self.user, self.openid)
